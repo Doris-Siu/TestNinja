222b4a6f6016d3e320b6f69ff728046f82be24f0
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-04-27 22:56:26 +0100

    simple unit test

---
+++ TestNinja/TestNinja_UnitTests/MathTests.cs
@@ -0,0 +1,26 @@
+﻿using TestNinjaDotNetSeven.Fundamentals;
+
+namespace TestNinja_UnitTests;
+
+public class MathTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Add_WhenCalled_ReturnSumOfAgruments()
+    {
+        // Arrange
+        var math = new TestNinjaDotNetSeven.Fundamentals.Math();
+
+        // Act
+        var result = math.Add(1, 2);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    
+}
