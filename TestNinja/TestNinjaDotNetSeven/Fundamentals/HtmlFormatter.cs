c09374e4caa32ae5b375a6c8c12f57aa27e62a98
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-04-27 20:27:18 +0100

    practice NUnit Test with dotnet7

---
+++ TestNinja/TestNinjaDotNetSeven/Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNinjaDotNetSeven.Fundamentals
+{
+    public class HtmlFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
