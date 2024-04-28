7d185cd0d2e1b21214d4a90cc3e43721c4875052
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-04-28 11:27:10 +0100

    revise SetUp attribute, blackbox testing

--- TestNinja/TestNinja_UnitTests/MathTests.cs
+++ TestNinja/TestNinja_UnitTests/MathTests.cs
@@ -4,23 +4,55 @@ namespace TestNinja_UnitTests;
 
 public class MathTests
 {
+    private TestNinjaDotNetSeven.Fundamentals.Math _math;
+
     [SetUp]
     public void Setup()
     {
+        _math = new TestNinjaDotNetSeven.Fundamentals.Math();
     }
 
     [Test]
     public void Add_WhenCalled_ReturnSumOfAgruments()
     {
         // Arrange
-        var math = new TestNinjaDotNetSeven.Fundamentals.Math();
+        //var math = new TestNinjaDotNetSeven.Fundamentals.Math();
 
         // Act
-        var result = math.Add(1, 2);
+        var result = _math.Add(1, 2);
 
         // Assert
         Assert.That(result, Is.EqualTo(3));
+        
+        
     }
 
-    
+    [Test]
+    public void Max_FirstAgrumentIsGreater_ReturnFirstAgrument()
+    {
+        var result = _math.Max(2, 1);
+
+        Assert.That(result, Is.EqualTo(2));
+
+    }
+
+    [Test]
+    public void Max_SecondAgrumentIsGreater_ReturnSecondAgrument()
+    {
+        var result = _math.Max(1, 2);
+
+        Assert.That(result, Is.EqualTo(2));
+
+    }
+
+    [Test]
+    public void Max_AgrumentsAreEqual_ReturnSameAgrument() // edge case
+    {
+        var result = _math.Max(1, 1);
+
+        Assert.That(result, Is.EqualTo(1));
+
+    }
+
+
 }
