91458c4e9c8574002102ce9b2ece66bd15fd149c
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-04-28 17:03:25 +0100

    more test scenarios

---
+++ TestNinja/TestNinja_UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using TestNinjaDotNetSeven.Fundamentals;
+
+namespace TestNinja_UnitTests;
+
+public class CustomerControllerTests
+{
+    private CustomerController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _controller = new CustomerController();
+    }
+
+    [Test]
+    public void GetCustomer_IdIsZero_ReturnNotFound()
+    {
+
+        var result = _controller.GetCustomer(0);
+
+        // NotFound 
+        Assert.That(result, Is.TypeOf<NotFound>());
+
+        // NotFound or one of its derivatives 
+        // Assert.That(result, Is.InstanceOf<NotFound>());
+    }
+
+    [Test]
+    public void GetCustomer_IdIsNotZero_ReturnOk()
+    {
+        var result = _controller.GetCustomer(1);
+
+        // NotFound 
+        Assert.That(result, Is.TypeOf<Ok>());
+
+    }
+}
+
---
+++ TestNinja/TestNinja_UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,50 @@
+﻿using TestNinjaDotNetSeven.Fundamentals;
+
+namespace TestNinja_UnitTests;
+
+public class ErrorLoggerTests
+{
+    private ErrorLogger _logger;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = new ErrorLogger();
+
+    }
+
+    [Test]
+    public void Log_WhenCalled_SetTheLastErrorProperty()
+    {
+
+        _logger.Log("a");
+
+        Assert.That(_logger.LastError, Is.EqualTo("a"));
+    }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    public void Log_InvalidError_ThrowArgumentNullException(string error)
+    {
+        // use lambda function to test method throwing exception
+        Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Log_ValidError_RaiseErrorLoggedEvent()
+    {
+        var id = Guid.Empty;
+
+        // subscribe the event before ACT
+        _logger.ErrorLogged += (sender, args) => { id = args; };
+        _logger.Log("a");
+
+        Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+
+    }
+
+}
+
+
